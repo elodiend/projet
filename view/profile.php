e8f790da0e8d6e46bc889332fcf9bb250da69bfd
elodiend <elokyo@hotmail.fr>
2025-02-18 22:17:20 +0100

    commit

---
+++ view/profile.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GamePlay</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/profil.css">
+    <link href="https://fonts.cdnfonts.com/css/roboto-3" rel="stylesheet" />
+</head>
+
+<body>
+    <header>
+    <?php include("../template/navbar.php"); ?>
+    </header>
+    <main>
+    <div class="categorycontain">
+            <div class="title1">
+                <h3>Mon profil</h3>
+            </div>
+            <input type="submit" class="deconnexion" value="Déconnexion">
+
+        </div>
+        <div class="profilecontain">
+            <div class="profile">
+                <div class="leftpart">
+                    <div class="pseudo">
+                        <img src="../assets/Images/ac.jpg" alt="" width="100%">
+                        <h2>pseudo en php</h2>
+                    </div>
+
+                    <div class="infoprofile">
+                    <h2>Mes infos</h2>
+                    <hr>
+                    <ul>
+                        <li><b>Age :</b> php</li>
+                        <li><b>Genre :</b> php</li>
+                        <li><b>Mail :</b> php</li>
+                        <li><b>Niveau de jeu :</b> php</li>
+                        <li><b>Fréquence de jeu :</b> php</li>
+                        <li><b>Membre depuis le :</b> php </li>
+                        <li><b>Genres préférés :</b> php</li>
+                        <li><b>Plateforme(s) :</b> php </li>
+                    </ul>
+                    </div>
+                </div>
+                                
+                
+                <div class="rightpart">
+                <h2>Jeux favoris</h2>
+                <hr>
+                    <ul>
+                        <li>lien</li>
+                        <li>lien</li>
+                        <li>lien</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+       
+        <div class="createbtn">
+            <a href="#"><h2>Modifier mon profil</h2></a>
+        </div>
+          <br>
+    </main>
+
+    <?php include("../template/footer.php"); ?>
+
+    <script src="../assets/js/menuderoulant.js"></script>
+    <script src="../assets/js/regexinscription.js"></script>
+</body>
+
+</html>
